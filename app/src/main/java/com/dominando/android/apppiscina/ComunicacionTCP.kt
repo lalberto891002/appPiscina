b60ec5c0f52de43cc5c2c397428ef4f5baf1a05d
Luis <lalberto8910b@gmail.com>
2019-09-20 11:39:47 -0300

    commit tudo

---
+++ app/src/main/java/com/dominando/android/apppiscina/ComunicacionTCP.kt
@@ -0,0 +1,114 @@
+package com.dominando.android.apppiscina
+
+
+import android.os.Looper
+import android.widget.Toast
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.lang.Exception
+import java.lang.RuntimeException
+import java.net.InetAddress
+import java.net.Socket
+import kotlin.concurrent.thread
+import android.os.Handler
+
+//maneja la comunicacao completa envio e recepcao recibe o metodo para os atualizacao da UI e para o erro
+class ComunicacionTCP(ip:String,porta:Int,msgError:String ="Error opening Socket",msgTimeout:String ="Timeout comunication",callback: (String) -> Unit) {
+    private var IP = ip
+    private var PORTA = porta
+    private var tcpCLient :Socket?= null
+    private var bufferOut:PrintWriter? = null
+    private var bufferIn:BufferedReader? = null
+    private val errorMsg = msgError
+    private val errorTimeot:String =msgTimeout
+    private var detiene_lectura = false
+    private var contador_timeout = 0
+
+    init {
+        initSocket(callback)
+    }
+
+    fun enviar_pelo_socket(mensaje:String):Unit{
+        if(bufferOut!=null){
+            thread(start = true){
+                bufferOut?.println(mensaje)
+                bufferOut?.flush()
+            }
+        }
+    }
+
+    fun stopCLiente(){
+        detiene_lectura = true
+        Thread.sleep(150)
+        if(bufferOut!=null){
+            bufferOut?.flush()
+            bufferOut?.close()
+
+        }
+    }
+
+    private fun initSocket(callback: (String) -> Unit){
+        thread(start = true){
+            try {
+                tcpCLient = Socket(InetAddress.getByName(IP), PORTA)
+                bufferOut = PrintWriter(tcpCLient?.getOutputStream())
+                bufferIn = BufferedReader(InputStreamReader(tcpCLient?.getInputStream()))
+            }
+            catch (e:Exception){
+              tcpCLient = null
+                Handler(Looper.getMainLooper()).post{
+                    callback(errorMsg)
+                }
+            }
+        }
+    }
+
+    fun comenzar_listener(callback:(String)->Unit){
+        detiene_lectura = false
+        var mensaje = ""
+        thread (start = true,isDaemon = true){
+            while(!detiene_lectura) {
+                try {
+                    mensaje = bufferIn?.readLine() ?: ""
+                    if (mensaje != "") {
+                        contador_timeout = 0
+                        callback(mensaje)
+                    }
+                    Thread.sleep(100)
+                }
+                catch (ex:Exception){
+                    mensaje = errorMsg
+                }
+            }
+        }
+
+        thread(start = true,isDaemon = true) {
+            while (!detiene_lectura) {
+                contador_timeout++;
+                if (contador_timeout >= MAX_TIMEOUT_COUNT)
+                    time_out_function(callback)
+                Thread.sleep(1000)
+            }
+
+        }
+    }
+    fun detiene_timer_lectura(){
+        detiene_lectura = true
+    }
+
+    fun time_out_function(callback: (String) -> Unit){
+        detiene_timer_lectura()
+        Handler(Looper.getMainLooper()).post{
+            callback(errorTimeot)
+
+        }
+
+    }
+
+    companion object{
+        val MAX_TIMEOUT_COUNT = 60
+    }
+
+}
+
---
+++ app/src/main/java/com/dominando/android/apppiscina/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.dominando.android.apppiscina
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+
+class MainActivity() : AppCompatActivity() {
+    private var tcpClient:ComunicacionTCP? =null
+    var yaarranco = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        try {
+            tcpClient = ComunicacionTCP("172.16.2.232", 9002,"Not Socket Open"){mensaje->
+                Toast.makeText(this,mensaje,Toast.LENGTH_LONG).show() //siempre vem aqui es por error
+            }
+        }
+        catch (ex:Exception){
+            Toast.makeText(MainActivity@this,"Error abrindo socket",Toast.LENGTH_LONG).show()
+        }
+
+        button.setOnClickListener {
+            if (tcpClient != null) {
+                tcpClient?.enviar_pelo_socket("#CONNECT")
+
+                if(!yaarranco) {
+                    tcpClient?.comenzar_listener() { mensaje ->
+                        if (mensaje != "")
+                            leer_porta(mensaje)
+                    }
+                    yaarranco = true
+                }
+
+            }
+
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        var msg:String=""
+        tcpClient?.stopCLiente()
+
+
+    }
+
+    private fun leer_porta(mensaje:String){
+        Handler(Looper.getMainLooper()).post{
+            Toast.makeText(this,mensaje,Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+
+}
